849fba5f1e474de3b5432ac975c58a47b0ebd997
Herve <dockerlite@gmail.com>
2018-04-20 18:30:30 +0200

    replace redis connection

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ import time
 import os
 
 application = Flask(__name__)
-cache = redis.StrictRedis(host=os.environ.get("REDIS_SERVICE_HOST","NOT FOUND") , port=os.environ.get("REDIS_PORT","NOT_FOUND"), db=0)
+cache = redis.StrictRedis(host=os.environ.get("REDIS_SERVICE_HOST","NOT FOUND") , port=os.environ.get("REDIS_SERVICE_PORT_REDIS","NOT_FOUND"), db=0)
 
 def get_hit_count():
     retries = 5
